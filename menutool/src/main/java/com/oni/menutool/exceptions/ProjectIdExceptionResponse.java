38cf0a62e15d4138b4af28ccc98f9a35e7c46783
cjacqo <contact.jayden.cipriano@gmail.com>
2021-03-18 23:45:47 -0700

    ProjectService Object

---
+++ menutool/src/main/java/com/oni/menutool/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,21 @@
+package com.oni.menutool.exceptions;
+
+public class ProjectIdExceptionResponse {
+	
+	private String projectIdentifier;
+	
+	public ProjectIdExceptionResponse(String projectIdentifier) {
+		this.projectIdentifier = projectIdentifier;
+	}
+
+	public String getProjectIdentifier() {
+		return projectIdentifier;
+	}
+
+	public void setProjectIdentifier(String projectIdentifier) {
+		this.projectIdentifier = projectIdentifier;
+	}
+	
+	
+
+}
