5de9e138bc7db9d4420b4179d3f22a330433b9a6
cjacqo <contact.jayden.cipriano@gmail.com>
2021-03-18 19:44:28 -0700

    first commit

---
+++ menutool/src/main/java/com/oni/menutool/MenutoolApplication.java
@@ -0,0 +1,13 @@
+package com.oni.menutool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenutoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenutoolApplication.class, args);
+	}
+
+}
---
+++ menutool/src/test/java/com/oni/menutool/MenutoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oni.menutool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenutoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
