d47b2abe77a791875a1f8eabc74fdb9a46b0c18d
cjacqo <contact.jayden.cipriano@gmail.com>
2021-03-18 23:12:30 -0700

    MapValidationErrorService Object

--- menutool/src/main/java/com/oni/menutool/web/ProjectController.java
+++ menutool/src/main/java/com/oni/menutool/web/ProjectController.java
@@ -1,21 +1,18 @@
 package com.oni.menutool.web;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oni.menutool.domain.Project;
+import com.oni.menutool.services.MapValidationErrorService;
 import com.oni.menutool.services.ProjectService;
 
 @RestController
@@ -25,18 +22,15 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-		if (result.hasErrors()) {
-			
-			Map<String, String> errMap = new HashMap<>();
-			
-			for (FieldError err: result.getFieldErrors()) {
-				errMap.put(err.getField(), err.getDefaultMessage());
-			}
-			
-			return new ResponseEntity<Map<String, String>>(errMap, HttpStatus.BAD_REQUEST);
-		}
+		
+		ResponseEntity<?> errMap = mapValidationErrorService.MapValidationService(result);
+		
+		if (errMap != null) return errMap;
 		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
