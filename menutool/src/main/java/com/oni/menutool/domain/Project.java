d8b6038af0ff4218e5956b490502b3f24d13e731
cjacqo <contact.jayden.cipriano@gmail.com>
2021-03-18 22:58:10 -0700

    ProjectController Object

--- menutool/src/main/java/com/oni/menutool/domain/Project.java
+++ menutool/src/main/java/com/oni/menutool/domain/Project.java
@@ -12,6 +12,8 @@ import javax.persistence.PreUpdate;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 public class Project {
 
@@ -21,16 +23,23 @@ public class Project {
 	
 	@NotBlank(message = "Project name is required")
 	private String projectName;
+	
 	@NotBlank(message = "Project identifier is required")
 	@Size(min=4, max=5, message = "Please use 4 to 5 characters")
 	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
+	
 	@NotBlank(message = "Project description is required")
 	private String description;
+	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date start_date;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date end_date;
 	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date created_At;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_At;
 	
 	public Project() {}
--- menutool/src/main/java/com/oni/menutool/web/ProjectController.java
+++ menutool/src/main/java/com/oni/menutool/web/ProjectController.java
@@ -1,8 +1,15 @@
 package com.oni.menutool.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +26,18 @@ public class ProjectController {
 	private ProjectService projectService;
 	
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+		if (result.hasErrors()) {
+			
+			Map<String, String> errMap = new HashMap<>();
+			
+			for (FieldError err: result.getFieldErrors()) {
+				errMap.put(err.getField(), err.getDefaultMessage());
+			}
+			
+			return new ResponseEntity<Map<String, String>>(errMap, HttpStatus.BAD_REQUEST);
+		}
+		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 		
